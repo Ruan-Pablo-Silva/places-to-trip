d3c35cb753f0fa973f006b9a580df734661af5b8
Ruan-Pablo-Silva <silvaruanpablo8@gmail.com>
2024-08-31 19:54:47 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ChooseTripLocationsComponent} from "./choose-trip-locations/choose-trip-locations.component";
+import {UpdateDataComponent} from "./update-data/update-data.component";
+import {MainScreenComponent} from "./main-screen/main-screen.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainScreenComponent },
+  { path: 'update-data', component: UpdateDataComponent },
+  { path: 'choose-trip-locations', component: ChooseTripLocationsComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,29 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { UpdateDataComponent } from './update-data/update-data.component';
+import { ChooseTripLocationsComponent } from './choose-trip-locations/choose-trip-locations.component';
+import {FormsModule} from "@angular/forms";
+import { provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {MainScreenServiceService} from "./main-screen/main-screen-service.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainScreenComponent,
+    UpdateDataComponent,
+    ChooseTripLocationsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi())
+
   ],
   bootstrap: [AppComponent]
 })
