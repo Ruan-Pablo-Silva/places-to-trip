5faf2dabb01aaaaf19be7c781f85eecee7cd3cfe
Ruan-Pablo-Silva <silvaruanpablo8@gmail.com>
2024-09-22 11:14:37 -0300

    side bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ChooseTripLocationsComponent} from "./choose-trip-locations/choose-trip-locations.component";
-import {UpdateDataComponent} from "./update-data/update-data.component";
-import {MainScreenComponent} from "./main-screen/main-screen.component";
+import { ChooseTripLocationsComponent } from "./choose-trip-locations/choose-trip-locations.component";
+import { UpdateDataComponent } from "./update-data/update-data.component";
+import { MainScreenComponent } from "./main-screen/main-screen.component";
 
 const routes: Routes = [
-  { path: '', component: MainScreenComponent },
+  { path: '', component: MainScreenComponent },  // Default route
   { path: 'update-data', component: UpdateDataComponent },
   { path: 'choose-trip-locations', component: ChooseTripLocationsComponent }
-
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {MainScreenComponent} from './main-screen/main-screen.component';
-import {UpdateDataComponent} from './update-data/update-data.component';
-import {ChooseTripLocationsComponent} from './choose-trip-locations/choose-trip-locations.component';
-import {FormsModule} from "@angular/forms";
-import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {LeftSideBarComponent} from "./left-side-bar/left-side-bar.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { UpdateDataComponent } from './update-data/update-data.component';
+import { ChooseTripLocationsComponent } from './choose-trip-locations/choose-trip-locations.component';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LeftSideBarComponent } from './left-side-bar/left-side-bar.component';
+
+// Import the necessary Angular Material modules for sidenav
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import {MatIcon} from "@angular/material/icon";
+import { MatIconModule } from '@angular/material/icon';  // Import MatIconModule
 
 @NgModule({
   declarations: [
@@ -17,19 +24,24 @@ import {LeftSideBarComponent} from "./left-side-bar/left-side-bar.component";
     MainScreenComponent,
     UpdateDataComponent,
     ChooseTripLocationsComponent,
+    LeftSideBarComponent,  // Sidebar component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     MatSnackBarModule,
-    LeftSideBarComponent
+    MatSidenavModule,      // Module for sidenav
+    MatButtonModule,       // Module for button to toggle sidenav
+    MatListModule,
+    MatIcon,
+    MatIconModule,         // Icon module (added)
+    // Module for list of items in sidenav
   ],
   providers: [
     provideClientHydration(),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimationsAsync()
-
   ],
   bootstrap: [AppComponent]
 })
